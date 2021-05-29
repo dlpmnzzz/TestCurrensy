eb880a66df55d785708c03419fdddc374969b6ee
KuzichkinDA <dlpmnzzz@gmail.com>
2021-05-29 14:13:49 +0300

    add app class and layout resources

--- app/src/main/java/com/example/testcurrency/MainActivity.kt
+++ app/src/main/java/com/example/testcurrency/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.testcurrency
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
+import com.example.testcurrency.ui.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add(R.id.fragment_container, HomeFragment())
+            }
+        }
     }
 }
